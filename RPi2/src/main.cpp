7598d1416bc74e5c6cb10f5e02b32a66fef2933c
Dumitru Melenteanu <d.melenteanu@dekart.com>
2016-07-08 10:48:49 +0300

    Default to windowed video view with higher res

--- RPi2/src/main.cpp
+++ RPi2/src/main.cpp
@@ -4,7 +4,9 @@
 int main( ){
 
 	ofSetLogLevel(OF_LOG_VERBOSE);
-	ofSetupOpenGL(320,240, OF_WINDOW);			// <-------- setup the GL context
+	/* ofSetupOpenGL(1024, 768, OF_FULLSCREEN); */
+	// comment next line and uncomment previous to get fullscreen video
+	ofSetupOpenGL(640,240, OF_WINDOW);// <-------- setup the GL context
 	ofRunApp( new ofApp());
 
 }
